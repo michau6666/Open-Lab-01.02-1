2a1d04492c61e4205a3db249445c7c289edef8bc
Michal Bernát <113421230+michau6666@users.noreply.github.com>
2022-09-20 09:16:06 +0200

    Update Exercise.cs

--- Open-Lab-01.02/Exercise.cs
+++ Open-Lab-01.02/Exercise.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_01._02
 {
@@ -6,7 +6,7 @@ namespace Open_Lab_01._02
     {
         public int Sum(int a, int b)
         {
-            throw new NotImplementedException();
+            return a + b;
         }
     }
 }
